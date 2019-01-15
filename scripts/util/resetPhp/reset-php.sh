9e0ce33f9d1c5e0e19b89c2277490023052f3611
Blue Gaston <bbluegaston@gmail.com>
2019-01-15 08:30:42 -0800

    starting grammar scrambling tests

--- scripts/util/resetPhp/reset-php.sh
+++ scripts/util/resetPhp/reset-php.sh
@@ -3,6 +3,7 @@
 cp $POLYSCRIPT_PATH/util/resetPhp/zend_language_parser.y $PHP_SRC_PATH/Zend/
 cp $POLYSCRIPT_PATH/util/resetPhp/zend_language_scanner.l $PHP_SRC_PATH/Zend/
 cp -r $POLYSCRIPT_PATH/util/resetPhp/phar $PHP_SRC_PATH/ext/
+rm $POLYSCRIPT_PATH/scrambled.json
 
 if [[ $1 == "-revert" ]]; then
 	rm -rf $POLYSCRIPT_PATH/tests_ps
